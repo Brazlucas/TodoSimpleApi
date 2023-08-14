04fd7c54b531a46c23c5ec559308f911655f5a92
Lucas Braz <lukkascomics@gmail.com>
2023-08-14 20:32:48 -0300

    integrated with database

---
+++ src/main/java/com/lucasbraz/todosimple/TodoSimpleApplication.java
@@ -0,0 +1,11 @@
+package com.lucasbraz.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSimpleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TodoSimpleApplication.class, args);
+  }
+}

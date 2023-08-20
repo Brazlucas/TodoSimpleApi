79eaeb6eb5c0154f91697ddf56ffa9a18347965a
Lucas Braz <lukkascomics@gmail.com>
2023-08-19 23:46:02 -0300

    new model: User

---
+++ src/main/java/com/lucasbraz/todosimple/models/User.java
@@ -0,0 +1,10 @@
+package com.lucasbraz.todosimple.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+  public static final String TABLE_NAME = "";
+}

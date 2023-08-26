8a0cf8f71c7ebd5d4fcdbdb794db6faba27ccd3a
Lucas Braz <lukkascomics@gmail.com>
2023-08-25 23:05:00 -0300

    create UserRepository

---
+++ src/main/java/com/lucasbraz/todosimple/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.lucasbraz.todosimple.repositories;
+
+import com.lucasbraz.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { }
